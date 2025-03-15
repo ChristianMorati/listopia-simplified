8de9f8f60b9b015975f25c8298f31bdd8b502bcc
christianmorati <christianmorati3@gmail.com>
2025-03-15 12:50:27 -0300

    update github-pages infos added to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/vite_react_shadcn_ts",
+  base: "/listopia-simplified",
   plugins: [
     react(),
     mode === 'development' &&
